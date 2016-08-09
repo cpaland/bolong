d30c522fcec8643071c50a4e8ba6e79f7ad0c16a
rizoa <rizoamaskuero@gmail.com>
2016-08-09 08:22:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>About Us | Nova</title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <script src="//use.fontawesome.com/35ae1d8c5e.js"></script>
+
+
+  <link rel="shortcut icon" href="imgs/favicon.ico">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="imgs/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="imgs/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="imgs/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="imgs/apple-touch-icon-57-precomposed.png">
+
+</head>
+
+<body>
+
+  <header class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="fa fa-icon-bar"></span>
+          <span class="fa fa-icon-bar"></span>
+          <span class="fa fa-icon-bar"></span>
+        </a>
+        <a id="logo" class="pull-left" href="index.html"></a>
+        <div class="nav-collapse collapse pull-right">
+          <ul class="nav">
+            <li><a href="/">Home</a></li>
+            <li class="active"><a href="#about-us.html">About Us</a></li>
+            <li><a href="#services.html">Services</a></li>
+            <li><a href="#portfolio.html">Portfolio</a></li>
+            <li class="#dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="fa fa-icon-angle-down"></i></a>
+              <ul class="dropdown-menu">
+                <li><a href="#career.html">Career</a></li>
+                <li><a href="#blog-item.html">Blog Single</a></li>
+                <li><a href="#faq.html">FAQ</a></li>
+                <li><a href="#pricing.html">Pricing</a></li>
+                <li><a href="#404.html">404</a></li>
+                <li><a href="#typography.html">Typography</a></li>
+                <li><a href="#registration.html">Registration</a></li>
+                <li class="divider"></li>
+                <li><a href="#privacy.html">Privacy Policy</a></li>
+                <li><a href="#terms.html">Terms of Use</a></li>
+              </ul>
+            </li>
+            <li><a href="#blog.html">Blog</a></li>
+            <li><a href="#contact-us.html">Contact</a></li>
+            <li class="login">
+              <a data-toggle="modal" href="#loginForm"><i class="fa fa-sign-in"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <section class="title">
+    <div class="container">
+      <div class="row-fluid">
+        <div class="span6">
+          <h1>About Us</h1>
+        </div>
+        <div class="span6">
+          <ul class="breadcrumb pull-right">
+            <li><a href="/">Home</a> <span class="divider">/</span></li>
+            <li class="active">About Us</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="about-us" class="container main">
+    <div class="row-fluid">
+
+
+      <h1 class="center">Meet the Team</h1>
+      <hr>
+      <div class="row-fluid">
+      
+        <div class="span3">
+          <div class="box">
+            <p><img src="images/sample/team1.jpg" alt=""></p>
+            <h5>Leonard M. Spurlock</h5>
+            <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat.</p>
+            <a class="btn btn-social btn-facebook" href="#"><i class="fa fa-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="fa fa-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="fa fa-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+          </div>
+        </div>
+        
+      </div>
+      <p>&nbsp;</p>
+      <p></p>
+      <hr>
+
+  </section>
+
+  <section id="bottom" class="main">
+
+    <div class="container">
+
+      <div class="row-fluid">
+
+        <div class="span3">
+          <h4>ADDRESS</h4>
+          <ul class="unstyled address">
+            <li>
+              <i class="fa fa-icon-home"></i><strong>Address:</strong> 1032 Wayback Lane, Wantagh<br>NY 11793
+            </li>
+            <li>
+              <i class="fa fa-icon-envelope"></i>
+              <strong>Email: </strong>
+            </li>
+            <li>
+              <i class="fa fa-icon-globe"></i>
+              <strong>Website:</strong> www.domain.com
+            </li>
+            <li>
+              <i class="fa fa-icon-phone"></i>
+              <strong>Toll Free:</strong> 631-409-3105
+            </li>
+          </ul>
+        </div>
+
+
+        <div id="tweets" class="span3">
+          <h4>OUR COMPANY</h4>
+          <div>
+            <ul class="arrow">
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Support</a></li>
+              <li><a href="#">Terms of Use</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Copyright</a></li>
+              <li><a href="#">We are hiring</a></li>
+              <li><a href="#">Clients</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+          </div>
+        </div>
+
+
+        <div id="archives" class="span3">
+          <h4>ARCHIVES</h4>
+          <div>
+            <ul class="arrow">
+              <li><a href="#">December 2012 (1)</a></li>
+              <li><a href="#">November 2012 (5)</a></li>
+              <li><a href="#">October 2012 (8)</a></li>
+              <li><a href="#">September 2012 (10)</a></li>
+              <li><a href="#">August 2012 (29)</a></li>
+              <li><a href="#">July 2012 (1)</a></li>
+              <li><a href="#">June 2012 (31)</a></li>
+            </ul>
+          </div>
+        </div>
+
+
+      </div>
+
+  </section>
+
+
+  <footer id="footer">
+    <div class="container">
+      <div class="row-fluid">
+        <div class="span5 cp">
+          &copy; 2013 <a target="_blank" href="http://shapebootstrap.net/" title="Free Twitter Bootstrap WordPress Themes and HTML templates">ShapeBootstrap</a>. All Rights Reserved.
+        </div>
+
+        <div class="span6">
+          <ul class="social pull-right">
+            <li><a href="#"><i class="fa fa-icon-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-pinterest"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-linkedin"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-google-plus"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-youtube"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-tumblr"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-dribbble"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-rss"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-github-alt"></i></a></li>
+            <li><a href="#"><i class="fa fa-icon-instagram"></i></a></li>
+          </ul>
+        </div>
+        <div class="span1">
+          <a id="gototop" class="gototop pull-right" href="#"><i class="fa fa-icon-angle-up"></i></a>
+        </div>
+
+      </div>
+    </div>
+  </footer>
+
+
+  <div class="modal hide fade in" id="loginForm" aria-hidden="false">
+    <div class="modal-header">
+      <i class="fa fa-icon-remove" data-dismiss="modal" aria-hidden="true"></i>
+      <h4>Login Form</h4>
+    </div>
+
+    <div class="modal-body">
+      <form class="form-inline" action="index.html" method="post" id="form-login">
+        <input type="text" class="input-small" placeholder="Email">
+        <input type="password" class="input-small" placeholder="Password">
+        <label class="checkbox">
+<input type="checkbox"> Remember me
+</label>
+        <button type="submit" class="btn btn-primary">Sign in</button>
+      </form>
+      <a href="#">Forgot your password?</a>
+    </div>
+
+  </div>
+
+  <script src="js/jquery-1.9.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
