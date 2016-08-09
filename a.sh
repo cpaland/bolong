fad55997dcd7c88bf875148a5e44a09e7fbfd1a2
rizoa <rizoamaskuero@gmail.com>
2016-08-09 08:16:37 +0700

    first commit

--- a.sh
+++ a.sh
@@ -2,6 +2,5 @@
 git init
 git add .
 git commit -m "first commit"
-git remote add origin https://github.com/cpaland/bolong.git
-git push -u origin master --ucpaland
-
+git remote add origin ssh://github.com/cpaland/bolong.git
+git push -u origin master
